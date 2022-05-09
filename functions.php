c6cee357a904d996e64bf659da3a60f8921b0d9e
Jo Dickson <jogreybill@gmail.com>
2022-05-09 11:00:34 -0400

    Initial commit - updated placeholder content

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
-namespace MyProject\Theme;
+namespace StrategicPlan\Theme;
 
-define( 'MYPROJECT_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
+define( 'STRATEGICPLAN_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
 
 
 // Theme foundation
-include_once MYPROJECT_THEME_DIR . 'includes/config.php';
-include_once MYPROJECT_THEME_DIR . 'includes/meta.php';
+include_once STRATEGICPLAN_THEME_DIR . 'includes/config.php';
+include_once STRATEGICPLAN_THEME_DIR . 'includes/meta.php';
 
 // Add other includes to this file as needed.
--- includes/config.php
+++ includes/config.php
@@ -2,11 +2,11 @@
 /**
  * Handle all theme configuration here
  **/
-namespace MyProject\Theme\Includes\Config;
+namespace StrategicPlan\Theme\Includes\Config;
 
 
-define( 'MYPROJECT_THEME_URL', get_stylesheet_directory_uri() );
-define( 'MYPROJECT_THEME_STATIC_URL', MYPROJECT_THEME_URL . '/static' );
-define( 'MYPROJECT_THEME_CSS_URL', MYPROJECT_THEME_STATIC_URL . '/css' );
-define( 'MYPROJECT_THEME_JS_URL', MYPROJECT_THEME_STATIC_URL . '/js' );
-define( 'MYPROJECT_THEME_IMG_URL', MYPROJECT_THEME_STATIC_URL . '/img' );
+define( 'STRATEGICPLAN_THEME_URL', get_stylesheet_directory_uri() );
+define( 'STRATEGICPLAN_THEME_STATIC_URL', STRATEGICPLAN_THEME_URL . '/static' );
+define( 'STRATEGICPLAN_THEME_CSS_URL', STRATEGICPLAN_THEME_STATIC_URL . '/css' );
+define( 'STRATEGICPLAN_THEME_JS_URL', STRATEGICPLAN_THEME_STATIC_URL . '/js' );
+define( 'STRATEGICPLAN_THEME_IMG_URL', STRATEGICPLAN_THEME_STATIC_URL . '/img' );
